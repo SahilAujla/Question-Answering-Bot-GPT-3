3359e753d8f50072998b61f779429119ebbb468e
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-12 17:56:56 +0530

    again minor fix

--- pages/api/open-ai.js
+++ pages/api/open-ai.js
@@ -6,26 +6,22 @@ function handler(req, res) {
 
   const prompt = req.body.message.trim();
 
-  try {
-    (async () => {
-      const gptResponse = await openai.answers({
-        documents: [],
-        question: `${prompt}`,
-        search_model: "ada",
-        model: "ada",
-        examples_context: "",
-        examples: [
-          ["What is human life expectancy in the United States?", "78 years."],
-        ],
-        max_tokens: 64,
-        stop: ["\n", "<|endoftext|>", "."],
-      });
+  (async () => {
+    const gptResponse = await openai.answers({
+      documents: [],
+      question: `${prompt}`,
+      search_model: "ada",
+      model: "ada",
+      examples_context: "",
+      examples: [
+        ["What is human life expectancy in the United States?", "78 years."],
+      ],
+      max_tokens: 64,
+      stop: ["\n", "<|endoftext|>", "."],
+    });
 
-      res.status(200).json({ answer: gptResponse.data.answers[0] });
-    })();
-  } catch (err) {
-    res.status(501).json({ answer: err.status });
-  }
+    res.status(200).json({ answer: gptResponse.data.answers[0] });
+  })();
 }
 
 export default handler;
