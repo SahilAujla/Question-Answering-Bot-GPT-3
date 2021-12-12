dfae1a7aa5f9af00dbde87bdc21ff7ea437c3432
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-12 18:21:57 +0530

    added the real code.

--- pages/api/open-ai.js
+++ pages/api/open-ai.js
@@ -6,22 +6,33 @@ function handler(req, res) {
 
   const prompt = req.body.message.trim();
 
-  (async () => {
-    const gptResponse = await openai.answers({
-      documents: [],
-      question: `${prompt}`,
-      search_model: "ada",
-      model: "ada",
-      examples_context: "",
-      examples: [
-        ["What is human life expectancy in the United States?", "78 years."],
-      ],
-      max_tokens: 64,
-      stop: ["\n", "<|endoftext|>", "."],
-    });
+  if (req.method === "POST") {
+    try {
+      (async () => {
+        const gptResponse = await openai.answers({
+          documents: [],
+          question: `${prompt}`,
+          search_model: "ada",
+          model: "ada",
+          examples_context: "",
+          examples: [
+            [
+              "What is human life expectancy in the United States?",
+              "78 years.",
+            ],
+          ],
+          max_tokens: 64,
+          stop: ["\n", "<|endoftext|>", "."],
+        });
 
-    res.status(200).json({ answer: gptResponse.data.answers[0] });
-  })();
+        res.status(200).json({ answer: gptResponse.data.answers[0] });
+      })();
+    } catch (err) {
+      res.status(500).json({});
+    }
+  } else {
+    res.status(500);
+  }
 }
 
 export default handler;
