2f0164542879d3124ad63fde6fd5473c4cd0c32c
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-12 17:52:53 +0530

    if else block from api route.

--- pages/api/open-ai.js
+++ pages/api/open-ai.js
@@ -1,5 +1,32 @@
 function handler(req, res) {
-  res.status(200).json({ message: "The serverless fuction works!" });
+  const OpenAI = require("openai-api");
+  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+  const openai = new OpenAI(OPENAI_API_KEY);
+
+  const prompt = req.body.message.trim();
+
+  try {
+    (async () => {
+      const gptResponse = await openai.answers({
+        documents: [],
+        question: `${prompt}`,
+        search_model: "ada",
+        model: "ada",
+        examples_context: "",
+        examples: [
+          ["What is human life expectancy in the United States?", "78 years."],
+        ],
+        max_tokens: 64,
+        stop: ["\n", "<|endoftext|>", "."],
+      });
+
+      res.status(200).json({ answer: gptResponse.data.answers[0] });
+      res.end();
+    })();
+  } catch (err) {
+    res.status(500).json({ answer: err.status });
+  }
 }
 
 export default handler;
