20c196fe74f12de3ac8b556fec8d462873d6672a
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-12 18:25:14 +0530

    latest code

--- pages/api/open-ai.js
+++ pages/api/open-ai.js
@@ -2,9 +2,13 @@ function handler(req, res) {
   const OpenAI = require("openai-api");
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
+  console.log(OPENAI_API_KEY);
+
   const openai = new OpenAI(OPENAI_API_KEY);
 
   const prompt = req.body.message.trim();
+
+  console.log(prompt);
 
   if (req.method === "POST") {
     try {
@@ -25,10 +29,16 @@ function handler(req, res) {
           stop: ["\n", "<|endoftext|>", "."],
         });
 
+        console.log(gptResponse.data.answers[0]);
+
         res.status(200).json({ answer: gptResponse.data.answers[0] });
       })();
     } catch (err) {
-      res.status(500).json({});
+      console.log(err);
+      console.log(err.status);
+      console.log(err.status());
+
+      res.status(500).json({ answer: err.status });
     }
   } else {
     res.status(500);
