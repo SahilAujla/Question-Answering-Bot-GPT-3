c024a238c53c3ad230ea4585c2967d5087bd7287
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-12 17:46:42 +0530

    Simplified the api route code.

--- pages/api/open-ai.js
+++ pages/api/open-ai.js
@@ -1,17 +1,5 @@
 function handler(req, res) {
-  const OpenAI = require("openai-api");
-  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-
-  const openai = new OpenAI(OPENAI_API_KEY);
-
-  const prompt = req.body.message.trim();
-
-  if (req.method === "POST") {
-    res.status(200).json({ message: "Hi my name is Sahil" });
-  } else {
-    res.status(200).json({ message: "The serverless fuction works!" });
-    res.end();
-  }
+  res.status(200).json({ message: "The serverless fuction works!" });
 }
 
 export default handler;
