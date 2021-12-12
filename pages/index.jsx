39a92f2ce0baa417896c00513dc4eb3968623c96
Sahil Aujla <sahilaujla15@gmail.com>
2021-12-12 15:47:40 +0530

    Added Styling.

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,11 +26,41 @@ function Home() {
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <input type="text" ref={textInputRef} className="border-black border-2" />
-      <button>Send</button>
-      <h1>{output}</h1>
-    </form>
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+      <div className="h-3/4 w-3/4 bg-form-bg flex flex-col justify-center items-center rounded-3xl shadow-2xl">
+        <form
+          onSubmit={formSubmitHandler}
+          className="flex flex-col justify-center items-center h-3/4 w-3/4 mb-8"
+        >
+          <h1 className="text-white text-center text-4xl mb-12">
+            Question Answering Bot
+          </h1>
+          <div className="w-full">
+            <label htmlFor="question" className="text-white text-lg mb-2 block">
+              Your Question:
+            </label>
+            <input
+              type="text"
+              id="question"
+              ref={textInputRef}
+              className="w-full mb-2 block border focus:transition-all focus:border-input-outline focus:outline-none focus:shadow-md focus:shadow-input-shadow p-2 bg-form-bg text-white rounded-md"
+            />
+          </div>
+          <button className="block bg-white w-40 text-black rounded-md p-2 mb-10 self-end">
+            Ask
+          </button>
+          <div className="w-full">
+            <h1 id="Answer" className="text-white text-center text-3xl">
+              {`${output}` || "The Answer will appear here."}
+            </h1>
+          </div>
+        </form>
+        <h1 className="text-white">
+          Made with ❤ by{" "}
+          <a href="https://twitter.com/SahilAujla15">Sahil Aujla</a>
+        </h1>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "form-bg": "#111215",
+        "input-outline": "#27EE52",
+        "input-shadow": "#34e059",
+      },
+    },
   },
   plugins: [],
 };
